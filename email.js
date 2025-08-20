4da4b28b1748e34493de1456f0b1b5fe0aaeb93e
0111as211064.yashjain@gmail.com <0111as211064.yashjain@gmail.com>
2025-08-20 22:34:48 +0530

    New changes for nodemailer 1

--- email.js
+++ email.js
@@ -6,7 +6,7 @@ const { google } = require('googleapis');
 const oAuth2Client = new google.auth.OAuth2(
   process.env.CLIENT_ID,
   process.env.CLIENT_SECRET,
-  process.env.REDIRECT_URI // must match Google Cloud Console
+  process.env.REDIRECT_URI 
 );
 
 // Set your refresh token (long-lived)
